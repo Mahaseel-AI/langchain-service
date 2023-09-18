a44407012c4f3923017c2c70c4c5137b16176e86
Mina Sameh <mina.s.wadie@gmail.com>
2023-09-18 15:22:34 +0300

    update

---
+++ test.ts
@@ -0,0 +1,47 @@
+import "dotenv/config";
+
+import { ServiceBusClient } from "@azure/service-bus";
+
+const connectionString = process.env.SERVICE_BUS_CONNECTION_STRING || "";
+
+const sbClient = new ServiceBusClient(connectionString);
+
+const subscription = "langchain-service";
+const topic = "response";
+
+const yieldSender = sbClient.createSender("yield-estimation-request");
+const disesaseSendwer = sbClient.createSender("disease-detection-request");
+const receiver = sbClient.createReceiver(topic, subscription);
+
+const main = async () => {
+  console.log("start");
+  for (let i = 0; i < 10; i++) {
+    const message = {
+      body: {
+        id: i,
+        name: "test",
+      },
+      label: "test",
+    };
+    await yieldSender.sendMessages(message);
+    await disesaseSendwer.sendMessages(message);
+  }
+  console.log("end");
+};
+
+const myMessageHandler = async (message: any) => {
+  // your code here
+  console.log(`message.body: ${message.body}`);
+};
+const myErrorHandler = async (args: any) => {
+  console.log(
+    `Error occurred with ${args.entityPath} within ${args.fullyQualifiedNamespace}: `,
+    args.error,
+  );
+};
+receiver.subscribe({
+  processMessage: myMessageHandler,
+  processError: myErrorHandler,
+});
+
+main()
