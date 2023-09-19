c23c4e10f5e28d66f4e5770529c9977986d84ba5
ahmedkhaled4d <ahmedkhaled4d@gmail.com>
2023-09-19 14:08:56 +0300

    process.env.APP

--- src/services/yieldEstimation.service.ts
+++ src/services/yieldEstimation.service.ts
@@ -2,15 +2,23 @@ import { DaprClient } from "dapr-client";
 
 const DAPR_HOST = process.env.DAPR_HOST ?? "http://localhost";
 const DAPR_HTTP_PORT = process.env.DAPR_HTTP_PORT ?? "3500";
-const PUBSUB_NAME = "Mahaseel-bus";
-const PUBSUB_TOPIC = "chatbot-topic";
+const PUBSUB_NAME = process.env.APP_PUBSUB_NAME ?? "mahaseel-bus";
+const PUBSUB_TOPIC = process.env.APP_PUBSUB_TOPIC ?? "marketplace-request";
 
 export async function pushToAzureBus() {
-  const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT);
+  const client = new DaprClient(DAPR_HOST, DAPR_HTTP_PORT, 1);
   const payload = {
+    userId: "111111",
+    type: "query",
+    data: { product: "P-7581458" },
     orderId: 200,
   };
-  await client.pubsub.publish(PUBSUB_NAME, PUBSUB_TOPIC, payload);
+  const publish = await client.pubsub.publish(
+    PUBSUB_NAME,
+    PUBSUB_TOPIC,
+    payload
+  );
+  console.log(publish);
 }
 
 async function sleep(ms: number) {
